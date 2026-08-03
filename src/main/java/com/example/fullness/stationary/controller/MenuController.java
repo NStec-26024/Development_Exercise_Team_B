bbd5e0bdd0f0fc08f756f8cbc7b924e1b19d6846
NStec-26001 <nstec26_001@fullness.email>
2026-08-03 17:41:56 +0900

    first commit

---
+++ src/main/java/com/example/fullness/stationary/controller/MenuController.java
@@ -0,0 +1,15 @@
+package com.example.fullness.stationary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class MenuController {
+
+    @GetMapping
+    public String adminRoot() {
+        return "adminmenu";
+    }
+}
