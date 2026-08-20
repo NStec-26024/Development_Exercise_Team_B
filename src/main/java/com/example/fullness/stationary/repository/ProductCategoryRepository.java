a0be853014c9fafea77ec0561e293a9a77380341
NStec-26003 <nstec26_003@fullness.email>
2026-08-20 13:56:36 +0900

    余分なimport文を削除

--- src/main/java/com/example/fullness/stationary/repository/ProductCategoryRepository.java
+++ src/main/java/com/example/fullness/stationary/repository/ProductCategoryRepository.java
@@ -1,7 +1,5 @@
 package com.example.fullness.stationary.repository;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
 
 import com.example.fullness.stationary.entity.ProductCategory;
