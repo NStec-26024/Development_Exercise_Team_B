36da88639db810bcf600051bbbdb2523669eb674
NStec-26001 <nstec26_001@fullness.email>
2026-08-26 10:19:14 +0900

    issue修正

--- src/main/java/com/example/fullness/stationary/controller/AdminProductEditFormController.java
+++ src/main/java/com/example/fullness/stationary/controller/AdminProductEditFormController.java
@@ -67,7 +67,7 @@ public class AdminProductEditFormController {
      */
     @GetMapping("/{id}")
     public String showEditForm(
-            @PathVariable Integer id,
+            @PathVariable("id") Integer id,
             Model model,
             RedirectAttributes redirectAttributes) {
 
